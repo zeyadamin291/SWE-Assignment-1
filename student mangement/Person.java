fe1c11cd86b745d53688a68e9964959df5ee8d60
ahmedadelsayah <20240033@stud.fci-cu.edu.eg>
2026-03-01 16:26:12 +0200

    Add files via upload

---
+++ student mangement/Person.java
@@ -0,0 +1,16 @@
+// Person.java
+public class Person {
+
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
